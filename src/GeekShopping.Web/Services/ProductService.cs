0fed27c452d81c8c97381f369d3df68df4a0f6d3
Vitor Pereira <vitor-pcruz@hotmail.com>
2022-12-30 15:12:03 -0300

    Injeção de Dependência da Classe ProductService em Program

--- src/GeekShopping.Web/Services/ProductService.cs
+++ src/GeekShopping.Web/Services/ProductService.cs
@@ -6,9 +6,12 @@ namespace GeekShopping.Web.Services
 {
     public class ProductService : IProductService
     {
+        public const string BasePath = "api/v1/products";
+        
         private readonly HttpClient _client;
 
-        public const string BasePath = "api/v1/products";
+        public ProductService(HttpClient client) =>
+            _client = client ?? throw new ArgumentNullException(nameof(client));
 
         public async Task<IEnumerable<ProductModel>> FindAllProducts()
         {
@@ -24,17 +27,34 @@ namespace GeekShopping.Web.Services
 
         public async Task<ProductModel> CreateProduct(ProductModel model)
         {
+            var response = await _client.PostAsJson($"{BasePath}", model);
+
+            if (response.IsSuccessStatusCode)
+                return await response.ReadContentAs<ProductModel>();
+            else throw new Exception("Something went wrong when calling API");
+
             throw new NotImplementedException();
         }
 
         public async Task<ProductModel> UpdateProduct(ProductModel model)
         {
+            var response = await _client.PutAsJson($"{BasePath}", model);
+
+            if (response.IsSuccessStatusCode)
+                return await response.ReadContentAs<ProductModel>();
+            else throw new Exception("Something went wrong when calling API");
+
             throw new NotImplementedException();
         }
 
         public async Task<bool> DeleteProductById(long id)
         {
-            throw new NotImplementedException();
+            var response = await _client.DeleteAsync($"{BasePath}/{id}");
+
+            if (response.IsSuccessStatusCode)
+                return await response.ReadContentAs<bool>();
+
+            else throw new Exception("Something went wrong when calling API");
         }
     }
 }
