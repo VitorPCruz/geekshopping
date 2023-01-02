48d60bc226208a8785e2ceacb1dc2b4f6a00323d
Vitor Pereira <vitor-pcruz@hotmail.com>
2023-01-02 17:53:47 -0300

    feat: Adicionando Configurações de Banco de Dados e Segurança

--- src/GeekShopping.IdentityServer/Program.cs
+++ src/GeekShopping.IdentityServer/Program.cs
@@ -1,18 +1,52 @@
-var builder = WebApplication.CreateBuilder(args);
+using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.Model;
+using GeekShopping.IdentityServer.Model.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Duende.IdentityServer.Services;
 
-// Add services to the container.
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Default");
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<SQLServerContext>(
+    configuration => configuration.UseSqlServer(connectionString));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<SQLServerContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddIdentityServer(
+    options =>
+    {
+        options.Events.RaiseErrorEvents = true;
+        options.Events.RaiseInformationEvents = true;
+        options.Events.RaiseSuccessEvents = true;
+        options.Events.RaiseFailureEvents = true;
+        options.EmitStaticAudienceClaim = true;
+    })
+    .AddDeveloperSigningCredential()
+    .AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+    .AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+    .AddInMemoryClients(IdentityConfiguration.Clients)
+    .AddAspNetIdentity<ApplicationUser>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseIdentityServer();
 
 app.UseAuthorization();
 
